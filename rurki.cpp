7dc62f44588c7e9807b701f3e7c359644c33af6a
ostr00000 <ostr00000@gmail.com>
2017-01-08 21:18:18 +0100

    3

--- rurki.cpp
+++ rurki.cpp
@@ -48,15 +48,16 @@ public:
 class Fragment{
     int podzial;
     NajmniejszyElement** elem;
-
-
+    Pozycja fragm;
 public:
-    Fragment(int podzial):
-        podzial(podzial){
+    Fragment(int podzial,Pozycja fragm):
+        podzial(podzial),
+        fragm(fragm){
         elem=new NajmniejszyElement* [podzial];
         for(int i=0;i<podzial;i++){
             elem[i]=new NajmniejszyElement[podzial];
         }
+        zainicjuj();
     }
 
     ~Fragment(){
@@ -66,9 +67,41 @@ public:
         delete [] elem;
     }
 
-    void zainicjuj(Pozycja fragm){
-        int dlRzedu=podzial*2+1;
+    void wypisz(){
+         for(int j=0;j<podzial;j++){
+            for(int i=0;i<podzial;i++){
+                cout<<"element na pozycji ("<<i<<","<<j<<")"<<endl;
+                elem[j][i].wypisz();
+                cout<<endl;
+            }
+         }
+    }
+    void wypiszRzad(int n){
+        if(n!=podzial){
+            for(int i=0;i<podzial;i++){
+                cout.width(2);
+                cout<<elem[i][n].get(lewoGora)<<" ";
+            }
+            if(prawoGora==fragm or prawoDol==fragm){
+                cout.width(2);
+                cout<<elem[podzial-1][n].get(prawoGora);
+            }
+        }else{
+            n--;
+            for(int i=0;i<podzial;i++){
+                cout.width(2);
+                cout<<elem[i][n].get(lewoDol)<<" ";
+            }
+            if(prawoGora==fragm or prawoDol==fragm){
+                cout.width(2);
+                cout<<elem[podzial-1][n].get(prawoDol);
+            }
+        }
+    }
+private:
+    void zainicjuj(){
         if(lewoGora==fragm){
+            int dlRzedu=podzial*2+1;
             for(int j=0;j<podzial;j++){
                 for(int i=0;i<podzial;i++){
                     elem[i][j].set(i+1+dlRzedu*j,lewoGora);
@@ -79,6 +112,7 @@ public:
 
             }
         }else if(prawoGora==fragm){
+            int dlRzedu=podzial*2+1;
             for(int j=0;j<podzial;j++){
                 for(int i=0;i<podzial;i++){
                     elem[i][j].set(podzial+i+1+dlRzedu*j,lewoGora);
@@ -87,17 +121,18 @@ public:
                     elem[i][j].set(podzial+i+2+dlRzedu*(j+1),prawoDol);
                 }
             }
+        }else if(prawoDol){
+            int przesuniecieStartowe=(2*podzial+1)*podzial+podzial;
+            for(int j=0;j<podzial;j++){
+                for(int i=0;i<podzial;i++){
+                    elem[i][j].set(przesuniecieStartowe+i+1+(podzial+1)*j,lewoGora);
+                    elem[i][j].set(przesuniecieStartowe+i+2+(podzial+1)*j,prawoGora);
+                    elem[i][j].set(przesuniecieStartowe+i+1+(podzial+1)*(j+1),lewoDol);
+                    elem[i][j].set(przesuniecieStartowe+i+2+(podzial+1)*(j+1),prawoDol);
+                }
+            }
         }else throw exception();
 
-    }
-    void wypisz(){
-         for(int j=0;j<podzial;j++){
-            for(int i=0;i<podzial;i++){
-                cout<<"element na pozycji ("<<i<<","<<j<<")"<<endl;
-                elem[j][i].wypisz();
-                cout<<endl;
-            }
-         }
     }
 };
 
@@ -108,12 +143,33 @@ public:
 
     Powierzchnia(int podzial):
         podzial(podzial){
-            a=new Fragment(podzial);
-            a->zainicjuj(lewoGora);
+            a=new Fragment(podzial,lewoGora);
+            b=new Fragment(podzial,prawoGora);
+            c=new Fragment(podzial,prawoDol);
     }
     void wypisz(){
         cout<<"fragment a"<<endl;
         a->wypisz();
+        cout<<"fragment b"<<endl;
+        b->wypisz();
+        cout<<"fragment c"<<endl;
+        c->wypisz();
+        cout<<endl;
+    }
+    void wypiszPlansze(){
+        cout<<endl;
+        for(int i=0;i<=podzial;i++){
+            a->wypiszRzad(i);
+            b->wypiszRzad(i);
+            cout<<endl;
+        }
+        for(int i=1;i<=podzial;i++){
+            for(int j=0;j<podzial;j++){
+                cout<<"   ";
+            }
+            c->wypiszRzad(i);
+            cout<<endl;
+        }
     }
 };
 
@@ -127,7 +183,8 @@ int main(int argc,char** argv){
     }
     cout<<"liczbaPodzialow "<<liczbaPodzialow<<endl;
     Powierzchnia *p=new Powierzchnia(liczbaPodzialow);
-    p->wypisz();
+    //p->wypisz();
+    p->wypiszPlansze();
 
 
     return 0;
